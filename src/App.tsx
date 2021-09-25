aa9599f8cbd09f9f9b36618eea302c3c56fcc0ee
luan.borher <luan.borher@gmail.com>
2021-09-25 18:37:47 -0300

    Login design completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
+import "./Styles/teste.scss";
+
 import { AuthContext } from "./Context/AuthContext";
 import React from "react";
+import imageBack from "./Image/imageFundo.jpeg";
 
 function App() {
-  const user = React.useContext(AuthContext);
-  console.log(user);
+  const user = "";
+  const pass = "";
 
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div id="page-login">
+      <main>
+        <div className="logo">
+          <h1>MARVEL</h1>
+        </div>
+        <form>
+          <label className="title">Bem-vindo(a) de volta!</label>
+          <label className="sub-title">Acesse sua conta:</label>
+          <input type="email" placeholder="Usuário" value={user} />
+          <input type="email" placeholder="Senha" value={pass} />
+          <div className="options">
+            <div>Salvar login</div>
+            <div className="sublin">Esqueci a senha</div>
+          </div>
+          <button type="submit">Entrar</button>
+          <p>
+            Ainda não tem o login?<div>Cadastre-se</div>
+          </p>
+        </form>
+      </main>
+      <aside>
+        <img src={imageBack} alt="Avangers" />
+      </aside>
     </div>
   );
 }
