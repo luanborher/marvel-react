de2588342e9d479a2e4057149407549eaddefbdc
luan.borher <luan.borher@gmail.com>
2021-09-25 19:04:09 -0300

    organizing folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,8 @@
-import "./Styles/teste.scss";
-
-import { AuthContext } from "./Context/AuthContext";
+import { Login } from "./Pages/Login";
 import React from "react";
-import imageBack from "./Image/imageFundo.jpeg";
 
 function App() {
-  const user = "";
-  const pass = "";
-
-  return (
-    <div id="page-login">
-      <main>
-        <div className="logo">
-          <h1>MARVEL</h1>
-        </div>
-        <form>
-          <label className="title">Bem-vindo(a) de volta!</label>
-          <label className="sub-title">Acesse sua conta:</label>
-          <input type="email" placeholder="Usuário" value={user} />
-          <input type="email" placeholder="Senha" value={pass} />
-          <div className="options">
-            <div>Salvar login</div>
-            <div className="sublin">Esqueci a senha</div>
-          </div>
-          <button type="submit">Entrar</button>
-          <p>
-            Ainda não tem o login?<div>Cadastre-se</div>
-          </p>
-        </form>
-      </main>
-      <aside>
-        <img src={imageBack} alt="Avangers" />
-      </aside>
-    </div>
-  );
+  return <Login />;
 }
 
 export default App;
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,3 +1,35 @@
+import "./styles.scss";
+
+import imageBack from "../../Image/imageFundo.jpeg";
+
 export function Login() {
-  return;
+  const user = "";
+  const pass = "";
+
+  return (
+    <div id="page-login">
+      <main>
+        <div className="logo">
+          <h1>MARVEL</h1>
+        </div>
+        <form>
+          <label className="title">Bem-vindo(a) de volta!</label>
+          <label className="sub-title">Acesse sua conta:</label>
+          <input type="email" placeholder="Usuário" value={user} />
+          <input type="email" placeholder="Senha" value={pass} />
+          <div className="options">
+            <div>Salvar login</div>
+            <div className="sublin">Esqueci a senha</div>
+          </div>
+          <button type="submit">Entrar</button>
+          <p>
+            Ainda não tem o login?<div>Cadastre-se</div>
+          </p>
+        </form>
+      </main>
+      <aside>
+        <img src={imageBack} alt="Avangers" />
+      </aside>
+    </div>
+  );
 }
