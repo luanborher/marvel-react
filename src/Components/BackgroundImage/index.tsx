14253a52e6201a579bb4ea26eb6ac2dffa804305
Luan Viana <luan.borher@gmail.com>
2021-09-27 13:44:13 -0300

    Import font, adjusting animation

--- src/Components/BackgroundImage/index.tsx
+++ src/Components/BackgroundImage/index.tsx
@@ -1,6 +1,6 @@
 import { ImgBackground } from "../../Components/BackgroundImage/styles";
 import imageBack from "../../Image/imageFundo.jpeg";
 
-const Button: React.FC = () => <ImgBackground src={imageBack} alt="Avangers" />;
+const Button = () => <ImgBackground src={imageBack} alt="Avengers" />;
 
 export default Button;
